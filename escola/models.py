169e9582069fbe6abb20acfc8b156278945504f0
22408741-maximonunes <a22408741@alunos.ulht.pt>
2026-03-25 10:00:54 +0000

    Add Turma, Professor, and Aluno models

--- escola/models.py
+++ escola/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 
-class Escola(models.Model):
+# Create your models here.
+class Turma(models.Model):
+    nome = models.CharField(max_length=50)
+    ano = models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.nome
+
+class Professor(models.Model):
     nome = models.CharField(max_length=100)
-    endereco = models.CharField(max_length=200)
-    
+    disciplina = models.CharField(max_length=50, blank=True)
+    turmas = models.ManyToManyField(
+        Turma, 
+        related_name='professores'
+    )
+
+    def __str__(self):
+        return self.nome
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    turma = models.ForeignKey(
+        Turma, 
+        on_delete=models.CASCADE, 
+        related_name='alunos'
+    )
+
     def __str__(self):
         return self.nome
 # Create your models here.
