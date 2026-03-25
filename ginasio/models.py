9e06b5ef643195a75be1072c1b62f152cacf9ffe
22408741-maximonunes <a22408741@alunos.ulht.pt>
2026-03-25 10:06:01 +0000

    Add Treinador model and update Cliente and Sessao models

--- ginasio/models.py
+++ ginasio/models.py
@@ -1,9 +1,40 @@
 from django.db import models
 
-class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
-    plano = models.CharField(max_length=50)
+class Treinador(models.Model):
+    nome_completo = models.CharField(max_length=100)
+    area_especializacao = models.CharField(max_length=100, blank=True)
+    descricao = models.TextField(blank=True)
 
     def __str__(self):
-        return f"{self.nome} - {self.plano}"
-# Create your models here.
+        return f"PT: {self.nome_completo}"
+
+
+class Cliente(models.Model):
+    nome_completo = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    data_registo = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome_completo
+
+
+class Sessao(models.Model):
+    treinador = models.ForeignKey(
+        Treinador,
+        on_delete=models.CASCADE,
+        related_name='sessoes'
+    )
+    cliente = models.ForeignKey(
+        Cliente,
+        on_delete=models.CASCADE,
+        related_name='agendamentos'
+    )
+
+    data_sessao = models.DateField()
+    hora_sessao = models.TimeField()
+
+    class Meta:
+        unique_together = ('treinador', 'data_sessao', 'hora_sessao')
+
+    def __str__(self):
+        return f"{self.cliente} com {self.treinador} em {self.data_sessao} às {self.hora_sessao}"
