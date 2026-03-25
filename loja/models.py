55a26ca6622ee1f0bb89383239aef833ba7ce80a
22408741-maximonunes <a22408741@alunos.ulht.pt>
2026-03-25 10:03:53 +0000

    Refactor models and update Produto price field type

--- loja/models.py
+++ loja/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    preco = models.FloatField()
+# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.nome} - {self.preco}€"
-# Create your models here.
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.CharField(max_length=10)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    morada = models.CharField(max_length=200) # Morada única aqui mesmo
+
+    def __str__(self):
+        return self.nome
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido {self.id} de {self.cliente.nome}"
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.quantidade}x {self.produto.nome}"
